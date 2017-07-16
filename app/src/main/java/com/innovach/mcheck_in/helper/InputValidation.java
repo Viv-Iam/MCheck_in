9b1ff4e397424fe7b8ae12cb8b54652b0ff25b68
viv-iam <vivian.opondoh@gmail.com>
2017-07-16 10:54:53 +0300

    Create methods in User class *getId() *getName() *getEmail() *getPassword()

--- app/src/main/java/com/innovach/mcheck_in/helper/InputValidation.java
+++ app/src/main/java/com/innovach/mcheck_in/helper/InputValidation.java
@@ -1,8 +1,22 @@
 package com.innovach.mcheck_in.helper;
 
+import android.content.Context;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
+
 /**
  * Created by vivian on 13/07/17.
  */
 
 public class InputValidation {
+
+    private Context context;
+
+    public InputValidation(Context context) {
+        this.context = context;
+    }
+
+    public boolean isInputEditTextFilled(TextInputEditText textInputEditText, TextInputLayout textInputLayout, String message) {
+
+    }
 }
